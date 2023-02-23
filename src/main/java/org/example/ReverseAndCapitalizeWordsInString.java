b73941c6a2c451f0408760eb38876e169b288693
percyvega <percyvega@gmail.com>
2023-02-23 15:39:37 -0500

    Added comments 2

--- src/main/java/org/example/ReverseAndCapitalizeWordsInString.java
+++ src/main/java/org/example/ReverseAndCapitalizeWordsInString.java
@@ -2,14 +2,8 @@ package org.example;
 
 public class ReverseAndCapitalizeWordsInString {
 
-    // this method must use reverseWord
     public static String reverseAndCapitalizeWordsInString(String string) {
-        return reverseWord(string);
-    }
-
-    // use recursion
-    private static String reverseWord(String word) {
-        return word;
+        return string;
     }
 
 }
